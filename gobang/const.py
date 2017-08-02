ca3dcadc336b7121413e9a180ebf97fa36b4f3b7
zhangting <zhangting@fingerdance.cn>
2017-08-02 16:35:14 +0800

    refactor

---
+++ gobang/const.py
@@ -0,0 +1,15 @@
+from typing import TypeVar
+
+Color = TypeVar('Color', int)
+
+NONE = Color(0)
+BLACK = Color(1)
+WHITE = Color(2)
+
+# board size
+BOARD_WIDTH = 15
+BOARD_HEIGHT = 15
+
+# todo need a better name
+FIVE = 5
+
